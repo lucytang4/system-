d8db75c1b7d68eef41dd2e2cac79e434f2757958
Lucy <ltang@stuy.edu>
2017-01-10 11:07:31 -0500

    files

---
+++ 110.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  //server side
+  int sd;
+  int connection;
+  char buffer[200];
+  sd = socket(AF_INET,SOCK_STREAM,0);
+  struct sockaddr_in sock;
+  sock.sin_family = AF_INET;
+  sock.sin_addr.s_addr = INADDR_ANY;
+  sock.sin_port = htons(9001);
+  bind(sd,(struct sockaddr *)&sock,sizeof(sock));
+  listen(sd,1);
+
+  //create another main file for client side
+  /*
+int sd;
+char *host = "127.0.0.1";
+  sd = socket(AF_INET,SOCK_STREAM,0);
+  struct sockaddr_in sock;
+  sock.sin_family = AF_INET;
+  inet_aton(host,&(sock.sin_addr));
+  sock.sin_port = htons(9001);
+bind(sd,(struct sockaddr *)&sock,sizeof(sock));
+   */
+  
+  return 0;
+}
